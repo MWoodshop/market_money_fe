cb1c1ea7f64778bb0bed2f61f0ba8b75e3be531f
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-09-07 21:52:24 -0400

    Initial Commit

---
+++ app/controllers/markets_controller.rb
@@ -0,0 +1,9 @@
+class MarketsController < ApplicationController
+  def index
+    @markets = Market.all
+  end
+
+  def show
+    @market = Market.find(params[:id])
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+  # Landing Page
+  root 'welcome#index'
+
+  # Markets
+  resources :markets, only: %i[index show]
+end
