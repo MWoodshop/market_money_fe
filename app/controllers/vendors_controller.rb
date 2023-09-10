1487f976167c57e01e8154ff40e30ba41b9cadd8
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-09-10 14:31:40 -0400

    Feat: updates controller to use API instead of DB

---
+++ app/controllers/vendors_controller.rb
@@ -0,0 +1,22 @@
+class VendorsController < ApplicationController
+  include HTTParty
+  base_uri 'http://localhost:3000/api/v0'
+
+  def index
+    @vendors = Vendor.all
+  end
+
+  def show
+    response = self.class.get("/vendors/#{params[:id]}")
+    if response.code == 200
+      raw_vendor = JSON.parse(response.body)
+      if raw_vendor && raw_vendor['data'] && raw_vendor['data']['attributes']
+        @vendor = raw_vendor['data']['attributes'].merge('id' => raw_vendor['data']['id'])
+      else
+        render status: :unprocessable_entity, json: { error: 'Malformed API response' }
+      end
+    else
+      render status: response.code, json: { error: 'Unable to fetch vendor data' }
+    end
+  end
+end
