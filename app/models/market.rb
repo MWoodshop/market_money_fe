0e839d34add08bbd981f0004e9111f21c19ad83d
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-09-10 14:35:08 -0400

    Chore: commented out models as they were created for BE, should likely remove on refactor

--- app/models/market.rb
+++ app/models/market.rb
@@ -1,2 +1,6 @@
-class Market < ApplicationRecord
-end
+# class Market < ApplicationRecord
+#   has_many :market_vendors
+#   has_many :vendors, through: :market_vendors
+# end
+
+# ***** Models likely not needed as this is a FE only application. Delete on refactor ******
