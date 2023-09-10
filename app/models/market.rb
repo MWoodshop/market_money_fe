876a997fd7728c486de30c42e417993e8dfffc50
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-09-10 16:52:11 -0400

    Refactor: removes unused models

--- app/models/market.rb
+++
@@ -1,6 +0,0 @@
-# class Market < ApplicationRecord
-#   has_many :market_vendors
-#   has_many :vendors, through: :market_vendors
-# end
-
-# ***** Models likely not needed as this is a FE only application. Delete on refactor ******
